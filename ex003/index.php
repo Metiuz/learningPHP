73e17ffcf74bb49795483a16a68c1b2ae1d71bc7
Metiuz <matheusrangel3123@gmail.com>
2024-12-09 23:53:11 -0300

    initial commit
    initial commit

---
+++ ex003/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = hexadecimal 0b = binário 0 = Octal
+        // $num = 010;
+        // echo "O valor da variável é $num";
+
+        // $v = true;
+        // var_dump($v) //Testar o tipo da variável
+
+        // $num = (integer) 3e2; //3x10² coerção (troca o tipo da variável)
+        // echo "O valor é: $num";
+        // var_dump($num)
+
+        // $num = (integer) "950";
+        // var_dump($num)
+
+        // $vet = [5, 3, 1, 5, 1];
+        // var_dump($vet)
+
+        // class Pessoa {
+        //     private string $nome;
+        // }
+        // $p = new Pessoa;
+        // var_dump($p)
+    ?>
+</body>
+</html>
